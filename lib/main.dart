4ba9e1a92ca51157d1cd1d2f20f70b34656a8e9f
DELL <anjugupta7060@gmail.com>
2023-08-07 19:50:55 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp(
+      child: MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "User's App",
+    // home: SplashScreen(),
+  )));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key, required this.child});
+  final Widget child;
+
+  static void restartApp(BuildContext context) {
+    context.findAncestorStateOfType<_MyAppState>()!.restartApp();
+  }
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Key key = UniqueKey();
+  void restartApp() {
+    setState(() {
+      key = UniqueKey();
+    });
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return KeyedSubtree(
+      key: key,
+      child: widget.child,
+    );
+  }
+}
